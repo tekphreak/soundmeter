0a63ec88b4b8b7df3816f87f8af5120eaaa706b4
Tekphreak <michaelrhogan@gmail.com>
2026-04-28 23:19:39 -0400

    Add app/src/main/java/com/tekphreak/soundmeter/MainActivity.java

---
+++ app/src/main/java/com/tekphreak/soundmeter/MainActivity.java
@@ -0,0 +1,19 @@
+package com.tekphreak.soundmeter;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {}
